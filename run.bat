8450b17592709e133c06d34fca85802af60ef2fc
Andriy Misiuryk <andriy.misiuryk@systematic.com>
2017-10-11 14:38:52 +0200

    KSPDEV-5221: Changed .net compiler from version 4.0 to 4.5

--- run.bat
+++ run.bat
@@ -10,10 +10,10 @@ echo 2 - Token client
 Set /p option=
 if %option% == 1 (
 	rem This path should be updated appropriately.
-	call %windir%\Microsoft.NET\Framework\v4.0.30319\msbuild.exe DemoClient\DemoClient.sln
+	call "%programfiles(x86)%\MSBuild\14.0\Bin\MSBuild.exe" DemoClient\DemoClient.sln
 	DemoClient\DemoClient\bin\Debug\DemoClient.exe
 ) else if %option% == 2 (
 	rem This path should be updated appropriately.
-	call %windir%\Microsoft.NET\Framework\v4.0.30319\msbuild.exe DemoTokenClient\DemoTokenClient.sln
+	call "%programfiles(x86)%\MSBuild\14.0\Bin\MSBuild.exe" DemoTokenClient\DemoTokenClient.sln
 	DemoTokenClient\DemoTokenClient\bin\Debug\DemoTokenClient.exe
 )
