1a6ed30df88b373ae9ab8a0aa88603c7a1657d52
Lasse Balmer Hansen <lasse.balmer.hansen@systematic.com>
2017-11-02 13:40:59 +0100

    fix certificates to be located in user personal

--- run.bat
+++ run.bat
@@ -1,8 +1,11 @@
 @echo OFF
 echo Installing certificates...
-certutil -p wRFsRP63H3kNEhDU -importpfx "certificates\personal\client.pfx"
-certutil -addstore -f "TRUSTEDPEOPLE" "certificates\trustedPeople\kombit-sp-signing-test (funktionscertifikat).cer"
-certutil -addstore -f "TRUSTEDPEOPLE" "certificates\trustedPeople\sts.cer"
+
+certutil -addstore -user -f Root "certificates\trustedPeople\01_systemtest_vii_primary_ca.p7b"
+certutil -addstore -user -f Root "certificates\trustedPeople\02_systemtest_xix_ca_trust.cer"
+certutil -addstore -user -f My "certificates\trustedPeople\03_sts.cer"
+certutil -addstore -user -f My "certificates\trustedPeople\04_kombit_sp_signing_test.p7b"
+certutil -importpfx -user -p wRFsRP63H3kNEhDU "certificates\personal\05_kombit_sp_t_demo_serv_funktionscertifikat_.p12" 
 
 echo Please choose a client to execute:
 echo 1 - Context client
