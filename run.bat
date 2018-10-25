50d70a7c87dd76e362d12c5d28c3a5a8e4442216
Jesper Tholstrup Pedersen <jesper.tholstrup.pedersen@systematic.com>
2018-10-25 11:57:59 +0200

    Changed certificate lookup. Removed support for TLS v1.0 and v1.1.

--- run.bat
+++ run.bat
@@ -2,14 +2,14 @@
 setlocal EnableExtensions EnableDelayedExpansion
 echo Installing certificates...
 
-rem certutil -addstore -user -f Root "certificates\trustedPeople\01_systemtest_vii_primary_ca.p7b"
-rem certutil -addstore -user -f Root "certificates\trustedPeople\02_systemtest_xix_ca_trust.cer"
-rem certutil -addstore -user -f Root "certificates\trustedPeople\03_trust2408_oces_primary_ca.cer"
-rem certutil -addstore -user -f Root "certificates\trustedPeople\04_trust2408_oces_ca_ii.cer"
-rem certutil -addstore -user -f Root "certificates\trustedPeople\05_trust2408_oces_ca_iii.cer"
-rem certutil -addstore -user -f My "certificates\trustedPeople\06_sts.cer"
-rem certutil -addstore -user -f My "certificates\trustedPeople\07_kombit_sp_signing_test.p7b"
-rem certutil -importpfx -user -p wRFsRP63H3kNEhDU "certificates\personal\08_kombit_sp_t_demo_serv_funktionscertifikat_.p12"
+certutil -addstore -user -f Root "certificates\trustedPeople\01_systemtest_vii_primary_ca.p7b"
+certutil -addstore -user -f Root "certificates\trustedPeople\02_systemtest_xix_ca_trust.cer"
+certutil -addstore -user -f Root "certificates\trustedPeople\03_trust2408_oces_primary_ca.cer"
+certutil -addstore -user -f Root "certificates\trustedPeople\04_trust2408_oces_ca_ii.cer"
+certutil -addstore -user -f Root "certificates\trustedPeople\05_trust2408_oces_ca_iii.cer"
+certutil -addstore -user -f My "certificates\trustedPeople\06_sts.cer"
+certutil -addstore -user -f My "certificates\trustedPeople\07_kombit_sp_signing_test.p7b"
+certutil -importpfx -user -p wRFsRP63H3kNEhDU "certificates\personal\08_kombit_sp_t_demo_serv_funktionscertifikat_.p12"
 
 rem This path should be updated appropriately.
 set "msbuild=%ProgramFiles(x86)%\Microsoft Visual Studio\2017\BuildTools\MSBuild\15.0\Bin\amd64\MSBuild.exe"
