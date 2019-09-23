3bf0c76b7768025c6ccb321f71840d5a273454cb
Jesper Tholstrup Pedersen <jesper.tholstrup.pedersen@systematic.com>
2019-09-23 14:22:07 +0200

    Issue #6 - Add intermediate certificate (trust2408_systemtest_xxii_ca)

--- run.bat
+++ run.bat
@@ -4,12 +4,13 @@ echo Installing certificates...
 
 certutil -addstore -user -f Root "certificates\trustedPeople\01_systemtest_vii_primary_ca.p7b"
 certutil -addstore -user -f Root "certificates\trustedPeople\02_systemtest_xix_ca_trust.cer"
-certutil -addstore -user -f Root "certificates\trustedPeople\03_trust2408_oces_primary_ca.cer"
-certutil -addstore -user -f Root "certificates\trustedPeople\04_trust2408_oces_ca_ii.cer"
-certutil -addstore -user -f Root "certificates\trustedPeople\05_trust2408_oces_ca_iii.cer"
-certutil -addstore -user -f My "certificates\trustedPeople\06_sts.cer"
-certutil -addstore -user -f My "certificates\trustedPeople\07_kombit_sp_signing_test_2022-05-21.cer"
-certutil -importpfx -user -p wRFsRP63H3kNEhDU "certificates\personal\08_kombit_sp_t_demo_serv_funktionscertifikat_.p12"
+certutil -addstore -user -f Root "certificates\trustedPeople\03_trust2408_systemtest_xxii_ca.cer"
+certutil -addstore -user -f Root "certificates\trustedPeople\04_trust2408_oces_primary_ca.cer"
+certutil -addstore -user -f Root "certificates\trustedPeople\05_trust2408_oces_ca_ii.cer"
+certutil -addstore -user -f Root "certificates\trustedPeople\06_trust2408_oces_ca_iii.cer"
+certutil -addstore -user -f My "certificates\trustedPeople\07_sts.cer"
+certutil -addstore -user -f My "certificates\trustedPeople\08_kombit_sp_signing_test.cer"
+certutil -importpfx -user -p wRFsRP63H3kNEhDU "certificates\personal\09_kombit_sp_t_demo_serv_funktionscertifikat_.p12"
 
 rem This path should be updated appropriately.
 set "msbuild=%ProgramFiles(x86)%\Microsoft Visual Studio\2017\BuildTools\MSBuild\15.0\Bin\amd64\MSBuild.exe"
